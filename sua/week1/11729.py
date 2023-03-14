399a0c04ddfdc82d83a290bd4c07a47293d74c54
Sua Kang <rkdtndk09@gmail.com>
2023-03-14 20:03:34 +0900

    chore: coding convention

--- sua/week1/11729.py
+++ sua/week1/11729.py
@@ -5,13 +5,14 @@ n = int(sys.stdin.readline())
 # 마지막 원판을 목표 장대에 놓는다.
 # 보조 장대에 있는 나머지 원판을 모두 목표 장대에 놓는다.
 
-def hanoi(n,start, target, temp):
+def hanoi(n, start, target, temp):
     if n == 1:
         print(start, target)
         return
     hanoi(n-1, start, temp, target)
     hanoi(1, start, target, temp)
     hanoi(n-1, temp, target, start)
+
 
 print(2**n - 1)
 hanoi(n, 1, 3, 2)
