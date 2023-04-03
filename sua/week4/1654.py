29f564e17eee6ee0fcd47d9000178c6841bb757b
Sua Kang <rkdtndk09@gmail.com>
2023-04-03 13:13:24 +0900

    [4주차] 7문제 / python - 강수아

---
+++ sua/week4/1654.py
@@ -0,0 +1,17 @@
+import sys
+
+k, n = map(int, sys.stdin.readline().split())
+l = [int(sys.stdin.readline()) for _ in range(k)]
+start, end = 1, max(l)
+
+while start <= end:
+    mid = (start + end) // 2
+    lines = 0  # 랜선 수
+    for i in l:
+        lines += i // mid
+
+    if lines >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
