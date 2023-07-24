0aaf27e30295eec74eec8c2db0d55ab1eded588d
Sua Kang <rkdtndk09@gmail.com>
2023-07-24 17:39:15 +0900

    [java] graph (1), swea 1문제

---
+++ sua/java/boj/graph/_24445.java
@@ -0,0 +1,72 @@
+package boj.graph;
+
+import java.io.*;
+import java.util.*;
+
+public class _24445 {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int[] visited;
+    static int trail;
+
+    public static void main(String[] args) throws IOException{
+        
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+
+        visited = new int[N + 1];
+        trail = 1;
+
+        for(int n = 0; n < N + 1; n++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int m = 0; m < M; m++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a); 
+        }
+
+        for(int i = 0; i < N; i++){
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+
+        bfs(R);
+
+        for(int i = 1; i < N+1; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void bfs(int node) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[node] = trail++;
+        q.add(node);
+
+        while(!q.isEmpty()) {
+            node = q.poll();
+
+            for(int i = 0; i < graph.get(node).size(); i++){
+                int nextNode = graph.get(node).get(i);
+
+                if(visited[nextNode] == 0) {
+                    visited[nextNode] = trail++;
+                    q.add(nextNode);
+                }
+            }
+        }
+
+    }
+}
