f629f501a4c0eb783e04e51b9bce59fc9c236e4c
Sua <rkdtndk09@gmail.com>
2023-08-03 17:55:09 +0900

    [java] 스택, 큐, 덱

---
+++ sua/java/boj/data_structure/_18258.java
@@ -0,0 +1,14 @@
+package boj.data_structure;
+
+import java.io.*;
+import java.util.*;
+
+public class _18258 {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+
+    public static void main(String[] args) {
+        
+    }
+}
